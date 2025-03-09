95eaf035933009cbeef526a2bb148df1d395d741
yavarmish <59411957+yavarmish@users.noreply.github.com>
2025-03-09 12:23:48 +0530

    Create DSU.cpp

---
+++ DSU.cpp
@@ -0,0 +1,55 @@
+/*
+    DSU
+    - findParent
+    - union(by rank/size)
+    
+    -- By rank(analogous to height, path compression might change height but not rank)
+    - Initial state : parent = node, rank = 0
+    - Find ultimate parent
+    - Connect smaller rank to larger one
+
+    -- By size
+    - Initial state : parent = node, size = 1
+    - Find ultimate parent
+    - Connect smaller size to larger one
+*/
+class DisjointSet {
+    vector<int> parent;
+    vector<int> rank;
+    vector<int> size;
+    public:
+    DisjointSet(int &n) {
+        parent.resize(n);
+        rank.resize(n, 0);
+        size.resize(n, 1);
+        for(int  i = 0; i < n; ++i)
+            parent[i] = i;
+    }
+    int findParent(int &u) {
+        if(parent[u] == u) return u;
+        else return parent[u] = findParent(parent[u]);
+    }        
+    void unionByRank(int &u, int &v) {
+        int upu = findParent(u), upv = findParent(v);
+        if(upu == upv) return;
+        if(rank[upv] == rank[upu]) ++rank[upu];
+        if(rank[upu] < rank[upv]) {
+            parent[upu] = upv;
+        }
+        else {
+            parent[upv] = upu;
+        }
+    }
+    void unionBySize(int &u, int &v) {
+        int upu = findParent(u), upv = findParent(v);
+        if(upu == upv) return;
+        if(size[upu] < size[upv]) {
+            parent[upu] = upv;
+            size[upv] += size[upu];
+        }
+        else {
+            parent[upv] = upu;
+            size[upu] += size[upv];
+        }
+    }
+};
